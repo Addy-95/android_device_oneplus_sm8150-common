95b467df77009a920c85fe91e80d20192b1d0f9c
Addy-95 <skabdullah.95@gmail.com>
2021-12-02 00:59:09 +0530

    sm8150-common: Colt-ify

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/TouchscreenGestureSettings.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/TouchscreenGestureSettings.java
@@ -37,8 +37,8 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreference;
 
-import com.android.internal.xtended.hardware.LineageHardwareManager; // Need FWB support
-import com.android.internal.xtended.hardware.TouchscreenGesture; // Need FWB support
+import com.android.internal.colt.hardware.LineageHardwareManager; // Need FWB support
+import com.android.internal.colt.hardware.TouchscreenGesture; // Need FWB support
 
 import org.lineageos.device.DeviceSettings.R;
 
